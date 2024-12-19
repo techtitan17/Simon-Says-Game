d46ec63e9b6b2a5708a34d07b65e461b122b0d4e
techtitan17 <1149111mk@gmail.com>
2024-12-19 17:59:18 +0530

    improved

--- simon.js
+++ simon.js
@@ -14,7 +14,7 @@ let level=0;
 let h2=document.querySelector("h2");
 
 
-document.addEventListener("keypress",function(){  //HOW TO GAME START
+document.addEventListener("keypress",function() {  //HOW TO GAME START
     if (started==false){
     console.log("game is started")
     started=true;
@@ -24,9 +24,9 @@ document.addEventListener("keypress",function(){  //HOW TO GAME START
 
 
 //HOW TO GAME LEVEL-UP AND BTN FLASHES;
-function gameflash(btn){
+function gameflash(btn) {
     btn.classList.add("flash");
-    setTimeout(() => {
+    setTimeout( function ()  {
         btn.classList.remove("flash");
     }, 250);
     };
@@ -57,12 +57,16 @@ function checkans(idx){
     // let idx=level-1;
     if(userseq[idx]===gameseq[idx]){
         if(userseq.length==gameseq.length){
-            setTimeout(levelup(),1000);
-        }else{
-            h2.innerText="Game over";
+            setTimeout(levelup,1000);
+        }}else{
+            h2.innerHTML=`Game over ! Your score was <b> ${level} </b> <br> Press any key to start.`;
+            document.querySelector("body").style.backgroundcolor = "red";
+            setTimeout(function() {
+                document.querySelector("body").style.backgroundcolor = "white";
+            },150);
             reset();
         }
-    }
+    
 
 }
 function btnpress(){
